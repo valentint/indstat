99c75abbac9f4e5bfc3dd055bda554160d62bee3
valentint <valentin.todorov@chello.at>
2022-06-14 01:58:59 +0200

    fixed issues "invalid multibyte string"

--- inst/create_rcodes.R
+++ inst/create_rcodes.R
@@ -1,6 +1,10 @@
 ##################################################################
 ##
 ##
+##  384=C<f4>te d'Ivoire
+##  638=R<e9>union
+##  531=Cura<e7>ao
+##
 create_rcodes <- function()
 {
     library(indstat)
@@ -12,6 +16,14 @@ create_rcodes <- function()
     rcodes$DESC <- enc2utf8(rcodes$DESC)
     raggr$ADESC <- enc2utf8(raggr$ADESC)
     raggr$CDESC <- enc2utf8(raggr$CDESC)
+
+    rcodes[rcodes$ACODE==531, "DESC"] <- "Curaçao"
+    rcodes[rcodes$ACODE==384, "DESC"] <- "Côte d'Ivoire"
+    rcodes[rcodes$ACODE==638, "DESC"] <- "Réunion"
+
+    raggr[raggr$CCODE==531, "CDESC"] <- "Curaçao"
+    raggr[raggr$CCODE==384, "CDESC"] <- "Côte d'Ivoire"
+    raggr[raggr$CCODE==638, "CDESC"] <- "Réunion"
 
     save(rcodes, raggr, file="country_codes.rda", version=2)
 }
